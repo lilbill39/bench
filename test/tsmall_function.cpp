2d860552a7a570ec81f6c4faa9edcaaef55a374e
ry <lilbill39@gmail.com>
2017-02-16 20:01:59 -0500

    More tests

--- test/tsmall_function.cpp
+++ test/tsmall_function.cpp
@@ -1,17 +1,40 @@
 #include "ilp.hpp"
 #include "gtest/gtest.h"
 
+namespace {
+  double localNullary() { return 1.2; };
+
+}
+
 TEST(small_function, nullary) {
   using namespace ILP;
 
   // Return by value
-  auto f = make_small_function([](){return 10;});
+  auto f = make_small_function([]() { return 10; });
   f();
   ASSERT_EQ(f(), 10);
 
+  // Fcn pointer
+  auto fp = make_small_function(localNullary);
+  ASSERT_EQ(fp(), localNullary());
+  
   // Void
   int n = 1;
-  auto fv = make_small_function([&n](){ ++n;});
+  auto fv = make_small_function([&n]() { ++n; });
   fv();
-  ASSERT_EQ(n,2);
+  ASSERT_EQ(n, 2);
+}
+
+TEST(small_function, unary) {
+  using namespace ILP;
+
+  // Pass POD by value
+  auto f = make_small_function([](int n) { return ++n; });
+  ASSERT_EQ(f(1), 2);
+
+  // Pass POD by reference
+  int n = 1;
+  auto fr = make_small_function([](int &x) { return ++x; });
+  ASSERT_EQ(fr(n), 2);
+  ASSERT_EQ(n, 2);
 }
