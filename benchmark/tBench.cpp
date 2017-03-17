57d90fc7d3751f4583c696a53386f4bae0e0cfb3
Ryan Livingston <lilbill39@gmail.com>
2017-03-17 17:36:43 -0400

    Add basic map

--- benchmark/tBench.cpp
+++ benchmark/tBench.cpp
@@ -1,5 +1,6 @@
 #include "ilp.hpp"
 #include <benchmark/benchmark.h>
+#include <vector>
 void bm_ilp(benchmark::State &aState) {
   while (aState.KeepRunning()) {
     auto res = ILP::ilp(3000UL);
@@ -37,5 +38,23 @@ void bm_function(benchmark::State &aState) {
 }
 
 BENCHMARK(bm_function);
+
+void bm_vector(benchmark::State &aState) {
+  //const size_t N = 10000U;
+  while (aState.KeepRunning()) {
+    std::vector<double> v;
+    v.reserve(6);
+    v.push_back(1.234);
+    v.push_back(2.234);
+    v.push_back(3.234);
+    v.push_back(3.234);
+    v.push_back(3.234);
+    v.push_back(3.234);        
+    benchmark::DoNotOptimize(v);
+    benchmark::ClobberMemory();
+  }
+}
+
+BENCHMARK(bm_vector);
 
 BENCHMARK_MAIN();
